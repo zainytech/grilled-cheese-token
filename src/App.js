435f766fb5b7ca14b0c857753b49a2e383259a37
Zainab Rasheed <zainabrasheed4142@gmail.com>
2023-04-12 16:52:10 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
+import { Helmet } from "react-helmet"
 import './App.css';
+import Home from './Comp/Home'
+import Routes from './Routes.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    {/* <Helmet>
+        <script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+          crossorigin="anonymous"
+          async
+        ></script>
+        <script src="/path/to/resource.js" type="text/javascript" />
+      </Helmet> */}
+
+    <div className="App">   
+    <Routes/>
     </div>
+    </>
   );
 }
 
